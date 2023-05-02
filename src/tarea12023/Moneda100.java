9a5e871559660fa7dcaec5d90ab0fa36ec306c8c
ingridddts <131918653+ingridddts@users.noreply.github.com>
2023-05-02 02:21:19 -0400

    Add files via upload
    actualizacion

---
+++ src/tarea12023/Moneda100.java
@@ -0,0 +1,13 @@
+package tarea12023;
+
+//clase que hereda de clase abstracta Moneda
+class Moneda100 extends Moneda{
+    public Moneda100(){
+        super();
+    }
+    
+    //devuelve su valor
+    public int getValor(){
+        return 100;
+    }
+}
